b3028881e07eb9eb3f026141847fe8978eef1c5d
zhenjia <zhenjiawang@gmail.com>
2018-06-05 23:47:22 +0800

    123

---
+++ store/src/com/mobius/providers/store/cal/CalSampleSpotSymbolWeightStore.java
@@ -0,0 +1,29 @@
+package com.mobius.providers.store.cal;
+
+
+import com.mobius.common.StoreException;
+import com.mobius.entity.cal.CalSampleSpotSymbolWeight;
+import org.guiceside.persistence.hibernate.dao.enums.Persistent;
+import org.guiceside.persistence.hibernate.dao.hquery.Selector;
+
+import java.util.List;
+
+
+public interface CalSampleSpotSymbolWeightStore {
+
+    CalSampleSpotSymbolWeight getById(Long id, Selector... selectors) throws StoreException;
+
+    CalSampleSpotSymbolWeight getBySymbolIdYearMonth(Long symbolId, Integer year, Integer month) throws StoreException;
+
+    List<CalSampleSpotSymbolWeight> getListByYearMonth(Integer year, Integer month) throws StoreException;
+
+    List<CalSampleSpotSymbolWeight> getList(List<Selector> selectorList) throws StoreException;
+
+    void save(CalSampleSpotSymbolWeight calSampleSpotSymbolWeight, Persistent persistent) throws StoreException;
+
+    void save(List<CalSampleSpotSymbolWeight> calSampleSpotSymbolWeightList, Persistent persistent) throws StoreException;
+
+    void delete(CalSampleSpotSymbolWeight calSampleSpotSymbolWeight) throws StoreException;
+
+    void deleteById(Long id) throws StoreException;
+}
---
+++ store/src/com/mobius/service/cal/CalSampleSpotSymbolWeightService.java
@@ -0,0 +1,76 @@
+package com.mobius.service.cal;
+
+import com.google.inject.Singleton;
+import com.mobius.common.StoreException;
+import com.mobius.entity.cal.CalSampleSpotSymbolWeight;
+import com.mobius.providers.store.cal.CalSampleSpotSymbolWeightStore;
+import org.guiceside.persistence.TransactionType;
+import org.guiceside.persistence.Transactional;
+import org.guiceside.persistence.hibernate.dao.enums.Persistent;
+import org.guiceside.persistence.hibernate.dao.hquery.HQuery;
+import org.guiceside.persistence.hibernate.dao.hquery.Selector;
+
+import java.util.List;
+
+/**
+ * Created by Lara Croft on 2016/12/21.
+ */
+@Singleton
+public class CalSampleSpotSymbolWeightService extends HQuery implements CalSampleSpotSymbolWeightStore {
+
+
+    @Transactional(type = TransactionType.READ_ONLY)
+    public CalSampleSpotSymbolWeight getById(Long id, Selector... selectors) throws StoreException {
+        return $(id, selectors).get(CalSampleSpotSymbolWeight.class);
+    }
+
+
+    @Transactional(type = TransactionType.READ_ONLY)
+    public List<CalSampleSpotSymbolWeight> getList(List<Selector> selectorList) throws StoreException {
+        return $(selectorList).list(CalSampleSpotSymbolWeight.class);
+    }
+
+    @Override
+    @Transactional(type = TransactionType.READ_ONLY)
+    public CalSampleSpotSymbolWeight getBySymbolIdYearMonth(Long symbolId, Integer year, Integer month) throws StoreException {
+        return $($eq("symbolId.id", symbolId), $eq("year", year), $eq("month", month)).get(CalSampleSpotSymbolWeight.class);
+    }
+
+    @Override
+    @Transactional(type = TransactionType.READ_ONLY)
+    public List<CalSampleSpotSymbolWeight> getListByYearMonth(Integer year, Integer month) throws StoreException {
+        return $($eq("year", year), $eq("month", month)).list(CalSampleSpotSymbolWeight.class);
+    }
+
+    /**
+     * 保存对象
+     */
+    @Transactional(type = TransactionType.READ_WRITE)
+    public void save(CalSampleSpotSymbolWeight calSampleSpotSymbolWeight, Persistent persistent) throws StoreException {
+        $(calSampleSpotSymbolWeight).save(persistent);
+    }
+
+    @Transactional(type = TransactionType.READ_WRITE)
+    public void save(List<CalSampleSpotSymbolWeight> calSampleSpotSymbolWeightList, Persistent persistent) throws StoreException {
+        $(calSampleSpotSymbolWeightList).save(persistent);
+    }
+
+    /**
+     * 删除对象
+     */
+    @Transactional(type = TransactionType.READ_WRITE)
+    public void delete(CalSampleSpotSymbolWeight calSampleSpotSymbolWeight) throws StoreException {
+        $(calSampleSpotSymbolWeight).delete();
+    }
+
+
+    /**
+     * 根据id 删除对象
+     *
+     * @param id
+     */
+    @Transactional(type = TransactionType.READ_WRITE)
+    public void deleteById(Long id) throws StoreException {
+        $(id).delete(CalSampleSpotSymbolWeight.class);
+    }
+}
